68d3fecea2f79af4e685dea6b420edc3ee58b8e4
Willian Mainieri <willianma@gmail.com>
2015-05-12 07:45:23 -0300

    entrada teste

--- Matlab/Entrada.m
+++ Matlab/Entrada.m
@@ -5,7 +5,73 @@ else
     a=x;
     fa=fx;
 end
+if ((x >= minVal) && (x <= maxVal) || (x == minVal))
+    b=x;
+    fb=fx;
+else
+    a=x;
+    fa=fx;
+end
+x = teste(12);
 
+i = 7;
+for i=1:r
+    k=1;
+end
+if ((x >= minVal) && (x <= maxVal) || (x == minVal))
+    b=x;
+    fb=fx;
+else
+    a=x;
+    fa=fx;
+end
+if ((x >= minVal) && (x <= maxVal) || (x == minVal))
+    b=x;
+    fb=fx;
+else
+    a=x;
+    fa=fx;
+end
+x = teste(12);
+
+i = 7;
+for i=1:r
+    k=1;
+end
+if ((x >= minVal) && (x <= maxVal) || (x == minVal))
+    b=x;
+    fb=fx;
+else
+    a=x;
+    fa=fx;
+end
+if ((x >= minVal) && (x <= maxVal) || (x == minVal))
+    b=x;
+    fb=fx;
+else
+    a=x;
+    fa=fx;
+end
+x = teste(12);
+
+i = 7;
+for i=1:r
+    k=1;
+end
+if ((x >= minVal) && (x <= maxVal) || (x == minVal))
+    b=x;
+    fb=fx;
+else
+    a=x;
+    fa=fx;
+end
+if ((x >= minVal) && (x <= maxVal) || (x == minVal))
+    b=x;
+    fb=fx;
+else
+    a=x;
+    fa=fx;
+end
 x = teste(12);
 
 i = 7;
