2255cdca3957ee7d18e3fcc3bafdfa1653a9786b
Willian Mainieri <willianma@gmail.com>
2015-05-19 01:58:07 -0300

    entrada teste 1

--- Matlab/Entrada.m
+++ Matlab/Entrada.m
@@ -1,14 +1,90 @@
+a=0;
 not(a > 3)
+and(a > 3)
 
+or(a > 3)
 a = (b)
-
 
 x = 10;
 minVal = 14;
 
+if (x==10)
+	x=x+1;
+end
+
+
+if ((x >= minVal) && (x <= maxVal))
+    x=x+1;
+elseif (x > maxVal)
+    x=x+1;
+	elseif (x==11)
+		x=x+2;
+	elseif (x==12)
+		x=x+3;
+	elseif (x==13)
+		x=x+4;
+	else
+		if (x<0)
+			x=x+6;
+		else
+			x=x+7;
+end
+
+if (x==10)
+	x=x+1;
+	elseif (x==11)
+		x=x+2;
+
+	end
+end
+
 a = (b + c - d * e / f^2)
 
+x = 10;
+minVal = 2;
+maxVal = 6;
 
+if ((x >= minVal) && (x <= maxVal))
+    x=x+1;
+elseif (x > maxVal)
+    x=x+1;
+else
+    x=x+1;
+end
+
+nc = 6;
+c = 1;
+nr = 4;
+i = 7;
+for i=1:r
+    k=1;
+    for i=1:r
+        k=1;
+    end
+end
+
+for c=1:nr
+	k=1;
+end
+
+while (x>minVal)
+	minval=minval+1;
+end
+
+while (1)
+	minval=2;
+end
+
+while x==1
+	minval=2;
+end
+
+n = 10;
+f = n;
+while (n > 1)
+    n = n-1;
+    f = f*n;
+end
 
 if (x >= minVal)
     a=x;
@@ -94,7 +170,57 @@ x = teste(12);
 i = 7;
 for i=1:r
     k=1;
-    for i=1:r
-        k=1;
+    for j=1:r
+		for k=1:r
+			for l=1:r
+				for m=1:r
+					for n=1:r
+						for o=1:r
+							k=k+1;
+						end
+					end
+				end
+			end
+		end
     end
+end
+
+n = 5;
+
+switch n
+    case -1
+        x = 5;
+		k = k+1;
+    case 0
+		switch n
+			case -1
+				switch n
+					case -1
+						x = 5;
+					case 0
+						x = 5;
+					case 1
+						x = 5;
+					otherwise
+						x = 5;
+				end
+			case 0
+				x = 5;
+			case 1
+				x = 5;
+			otherwise
+				x = 5;
+		end
+    case 1
+        x = 5;
+    otherwise
+        x = 5;
+end
+switch n
+    case -1
+        x = 5;
+    case 0
+        x = 5;
+    case 1
+        x = 5;
 end
